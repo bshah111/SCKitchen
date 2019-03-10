ae4f735641f00d02a580bf53d85625e5d42b79f0
Nancyb <nancyeb27@yahoo.com>
2019-03-10 15:17:55 -0600

    added email file

---
+++ controller/send-email.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+const crypt = require('./crypt');
+const encryptedPassword = '126891c03fef8f5fc1e1d8714d82bb';
+const transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'thisdavejdemo@gmail.com',
+        pass: crypt.decrypt(encryptedPassword),
+    },
+});
+module.exports = function sendEmail(to, subject, message) {
+    const mailOptions = {
+        from: 'thisdavejdemo@gmail.com',
+        to,
+        subject,
+        html: message,
+    };
+    transport.sendMail(mailOptions, (error) => {
+        if (error) {
+            console.log(error);
+        }
+        // console.log(`Message sent: ${info.response}`);
+    });
+};
+
+
+
+// const sendEmail = require('./send-email');
+// function sendNotification(message) {
+//     sendEmail('thisdavej@gmail.com, john@doe.com', 'Meals  available  notification', message);
+//     console.log('Email sent');
+// }
