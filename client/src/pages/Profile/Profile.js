410c252221420d2d3eee93e06b936f9501011427
ashackn <ashackn@gmail.com>
2019-03-13 19:18:41 -0600

    database

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react";
+import { Form, FormGroup, Label, Input, FormText, Alert } from 'reactstrap';
 import "./Profile.scss";
 import { Button } from "reactstrap";
 import { Link } from "react-router-dom"
 import API from "../../utils/API"
+
 
 class Profile extends Component {
     state = {
@@ -43,8 +45,38 @@ class Profile extends Component {
                 {this.state.loggedIn ? (
                     <div className="profileBox">
                         <h1 id="userTitle">Welcome {this.state.user.username}</h1>
+                        <div>
+            <Form>
+                <FormGroup>
+                    <Label for="locname">Location Name</Label>
+                    <Input type="text" name="locname" id="locname" placeholder="Location Name" value={this.props.locname} onChange={this.props.handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="poc">Person of Contact</Label>
+                    <Input type="text" name="poc" id="poc" placeholder="Person of Contact" value={this.props.poc} onChange={this.props.handleInputChange}  />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="foodinfo">Food Information</Label>
+                    <Input type="text" name="foodinfo" id="foodinfo" placeholder="Food Information" value={this.props.foodinfo} onChange={this.props.handleInputChange}  />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="meals">Meals</Label>
+                    <Input type="text" name="meals" id="meals" placeholder="Number of meals" value={this.props.meals} onChange={this.props.handleInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="pickup">Pick Up</Label>
+                    <Input type="text" name="pickup" id="pickup" placeholder="Pick Up Instructions" value={this.props.pickup} onChange={this.props.handleInputChange}  />
+                </FormGroup>
+
+            </Form>
+        </div>
                     </div>
-                ) : (
+                    
+                ) : 
+                
+
+
+                (
                     <div className="noUser">
                         {!this.state.loading ? (
                             <>
@@ -57,9 +89,46 @@ class Profile extends Component {
                     </div> 
                 )}
             </div>
+
+
         )
+
+
     }
 }
 
+
+
+    // //  render() 
+    //      return (
+        // <div>
+        //     <Form>
+        //         <FormGroup>
+        //             <Label for="locname">Location Name</Label>
+        //             <Input type="text" name="locname" id="locname" placeholder="Location Name" value={this.props.locname} onChange={this.props.handleInputChange} />
+        //         </FormGroup>
+        //         <FormGroup>
+        //             <Label for="poc">Person of Contact</Label>
+        //             <Input type="text" name="poc" id="poc" placeholder="Person of Contact" value={this.props.poc} onChange={this.props.handleInputChange}  />
+        //         </FormGroup>
+        //         <FormGroup>
+        //             <Label for="foodinfo">Food Information</Label>
+        //             <Input type="text" name="foodinfo" id="foodinfo" placeholder="Food Information" value={this.props.foodinfo} onChange={this.props.handleInputChange}  />
+        //         </FormGroup>
+        //         <FormGroup>
+        //             <Label for="meals">Meals</Label>
+        //             <Input type="text" name="meals" id="meals" placeholder="Number of meals" value={this.props.meals} onChange={this.props.handleInputChange} />
+        //         </FormGroup>
+        //         <FormGroup>
+        //             <Label for="pickup">Pick Up</Label>
+        //             <Input type="text" name="pickup" id="pickup" placeholder="Pick Up Instructions" value={this.props.pickup} onChange={this.props.handleInputChange}  />
+        //         </FormGroup>
+            
+
+        //     </Form>
+        // </div>
+    //     //  );
+         
+        
 
 export default Profile;
