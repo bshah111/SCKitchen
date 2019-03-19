4cbc45f07c867bf1c31f2db2f5721340bb110516
Nancyb <nancyeb27@yahoo.com>
2019-03-18 20:43:07 -0600

    add email

--- controller/mealController.js
+++ controller/mealController.js
@@ -1,5 +1,21 @@
 const db = require("../models");
+const nodemailer = require("nodemailer");
+const transport = nodemailer.createTransport({
+ service: 'Gmail',
+ port: 587,
+ secure: false,
+ auth: {
+   user: "SCKitchen1864@gmail.com",
+   pass: "duwomen24",
+ },
+});
 
+const mailOptions = {
+ from: '"Second Chance Kitchen" <SCKitchen1864@gmail.com>',
+ to: ["sbarenz@mac.com", "SCKitchen1864@yahoo.com", "nancyeb27@yahoo.com"],
+ subject: "SC Kitchen has a new listing!",
+ text: "Please come visit our website to see the new offering! Thanks much, SC Kitchen"
+};
 // Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
@@ -15,8 +31,17 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
-    db.Meal
+  create: function (req, res) {
+    console.log("here's the create stuff")
+    // pullEmails();
+    transport.sendMail(mailOptions, function (error, info) {
+      console.log("this is the transport business#1")
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+    });
+      db.Meal
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
